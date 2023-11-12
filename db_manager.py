c854337afd4edeb9eae41cb1681912ef8e31bd86
Chauncey <dongcl97@gmail.com>
2023-11-12 16:35:40 +0800

    add comment and support appending df

--- db_manager.py
+++ db_manager.py
@@ -1,23 +1,64 @@
 import sqlite3
 import pandas as pd
+from datetime import datetime
+
 
 class TreasuryDatabaseManager:
+    """
+    A class to manage the SQLite database for treasury data.
+    It handles creating tables, appending data, and reading data.
+    """
+
     def __init__(self, db_name="treasury_data.db"):
+        """
+        Initializes the database manager and connects to the SQLite database.
+        :param db_name: Name of the SQLite database file.
+        """
         self.conn = sqlite3.connect(db_name)
 
-    def create_table(self, df, table_name):
-        # 将DataFrame的列类型转换为SQLite兼容的类型
-        df.to_sql(table_name, self.conn, if_exists='replace', index=False)
+    def add_timestamp(self, df):
+        """
+        Adds a timestamp column to a DataFrame.
+        :param df: DataFrame to which the timestamp will be added.
+        :return: DataFrame with timestamp column.
+        """
+        df['timestamp'] = datetime.now().strftime("%Y-%m-%d")
+        return df
 
     def write_data(self, df, table_name):
-        # 写入数据到表
-        df.to_sql(table_name, self.conn, if_exists='replace', index=False)
+        """
+        Writes data to a specified table in the database. Appends data to existing table.
+        :param df: DataFrame containing the data to be written.
+        :param table_name: Name of the table in the database.
+        """
+        df = self.add_timestamp(df)
+        df.to_sql(table_name, self.conn, if_exists='append', index=False)
 
-    def read_data(self, table_name):
-        # 从表中读取数据
+    def read_data(self, table_name, date=None):
+        """
+        Reads data from a specified table. Can optionally filter data by a specific date.
+        :param table_name: Name of the table to read data from.
+        :param date: Specific date to filter data (format: 'YYYY-MM-DD').
+        :return: DataFrame with the read data.
+        """
         query = f"SELECT * FROM {table_name}"
+        if date:
+            query += f" WHERE timestamp = '{date}'"
         return pd.read_sql_query(query, self.conn)
 
     def close(self):
-        # 关闭数据库连接
+        """
+        Closes the connection to the SQLite database.
+        """
         self.conn.close()
+
+
+if __name__ == '__main__':
+    # Example usage:
+    # db_manager = TreasuryDatabaseManager()
+    # db_manager.write_data(df_bills, "bills")  # Assuming df_bills is a DataFrame you want to write
+    # db_manager.write_data(df_notes, "notes")  # Repeat for other DataFrames
+    # ...
+    # df_bills_nov12 = db_manager.read_data("bills", "2023-11-12")  # Reading data for a specific date
+    # db_manager.close()
+    pass
--- q3.py
+++ q3.py
@@ -2,8 +2,7 @@ from scraper import TreasuryDataScraper
 from db_manager import TreasuryDatabaseManager
 
 if __name__ == '__main__':
-
-    #
+    # scrape data
     scraper = TreasuryDataScraper()
     df_bills = scraper.get_df_bills()
     df_notes = scraper.get_df_notes()
@@ -12,19 +11,28 @@ if __name__ == '__main__':
     df_FRNs = scraper.get_df_FRNs()
     scraper.close_driver()
 
+    # cache to csv
+    df_bills.to_csv("treasury_cache/bills.csv", index=False)
+    df_notes.to_csv("treasury_cache/notes.csv", index=False)
+    df_Bonds.to_csv("treasury_cache/bonds.csv", index=False)
+    df_TIPS.to_csv("treasury_cache/tips.csv", index=False)
+    df_FRNs.to_csv("treasury_cache/frns.csv", index=False)
+
     # create database
     db_manager = TreasuryDatabaseManager()
 
     # create table
-    db_manager.create_table(df_bills, "bills")
-    db_manager.create_table(df_notes, "notes")
-    db_manager.create_table(df_Bonds, "bonds")
-    db_manager.create_table(df_TIPS, "tips")
-    db_manager.create_table(df_FRNs, "frns")
+    db_manager.write_data(df_bills, "bills")
+    db_manager.write_data(df_notes, "notes")
+    db_manager.write_data(df_Bonds, "bonds")
+    db_manager.write_data(df_TIPS, "tips")
+    db_manager.write_data(df_FRNs, "frns")
 
-    #
+    # read table
     df_bills_from_db = db_manager.read_data("bills")
+    print(df_bills_from_db)
     df_notes_from_db = db_manager.read_data("notes")
+    print(df_notes_from_db)
 
-    #
+    # close database
     db_manager.close()
